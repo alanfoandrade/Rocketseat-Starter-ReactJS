c61449645605ab44ec17b4390cc73ebe16d59e55
alanfoandrade <alanfrank@gec.inatel.br>
2019-10-30 21:09:28 -0300

    Add Header, Add busca na API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
-function App() {
+import Main from './pages/main';
+
+import './styles.css';
+
+import Header from './components/Header';
+
+export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Main />
     </div>
   );
 }
 
+/**
+ *
+ *
+ *
+ import React, { Component } from 'react';
+
+ class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
 export default App;
+ *
+ *
+ */
